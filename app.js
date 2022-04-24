aba2d9dc14c8c4acfc48a70ec4fc702977055120
jameshoo123 <hoosinyong@hotmail.com>
2022-04-25 00:23:58 +0800

    project init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { create } = require('express-handlebars')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const port = process.env.PORT || 3000
+const hbs = create({ defaultLayout: 'main', extname: '.hbs' })
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use(routes)
+
+app.listen(port, () => {
+  console.info(`App listening on port ${port}!`)
+})
+
+module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const restController = require('../controllers/restaurant-controllers')
+
+router.get('/restaurants', restController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/restaurants'))
+
+module.exports = router
