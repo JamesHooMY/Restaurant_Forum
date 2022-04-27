0cd7e1389c6de1d90a41ea407fff6b845c1285f3
jameshoo123 <hoosinyong@hotmail.com>
2022-04-27 17:06:39 +0800

    feat: modify header & add footer

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const { create } = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
-
+const { create } = require('express-handlebars')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
-const hbs = create({ defaultLayout: 'main', extname: '.hbs' })
+const hbs = create({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: handlebarsHelpers,
+})
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 
@@ -29,6 +34,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
