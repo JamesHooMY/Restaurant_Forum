0f79fd1d65ed959c335446bc29a21934c037c482
jameshoo123 <hoosinyong@hotmail.com>
2022-04-28 14:50:03 +0800

    feat: admin update restaurant

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -6,15 +6,6 @@ const adminController = {
       .then((restaurants) => res.render('admin/restaurants', { restaurants }))
       .catch((err) => next(err))
   },
-  getRestaurant: (req, res, next) => {
-    const restaurantId = req.params.id
-    return Restaurant.findByPk(restaurantId, { raw: true })
-      .then((restaurant) => {
-        if (!restaurant) throw new Error('Restaurant is not exist!')
-        res.render(`admin/restaurant`, { restaurant })
-      })
-      .catch((err) => next(err))
-  },
   createRestaurant: (req, res) => {
     res.render('admin/create-restaurant')
   },
@@ -29,7 +20,46 @@ const adminController = {
       description,
     })
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created')
+        req.flash('success_messages', 'restaurant was successfully created!')
+        res.redirect('/admin/restaurants')
+      })
+      .catch((err) => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    const restaurantId = req.params.id
+    return Restaurant.findByPk(restaurantId, { raw: true })
+      .then((restaurant) => {
+        if (!restaurant) throw new Error('Restaurant is not exist!')
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch((err) => next(err))
+  },
+  editRestaurant: (req, res) => {
+    const restaurantId = req.params.id
+    return Restaurant.findByPk(restaurantId, { raw: true })
+      .then((restaurant) => {
+        if (!restaurant) throw new Error('Restaurant is not exist!')
+        res.render('admin/edit-restaurant', { restaurant })
+      })
+      .catch((err) => next(err))
+  },
+  putRestaurant: (req, res) => {
+    const restaurantId = req.params.id
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    return Restaurant.findByPk(restaurantId)
+      .then((restaurant) => {
+        if (!restaurant) throw new Error('Restaurant is not exist!')
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully edited!')
         res.redirect('/admin/restaurants')
       })
       .catch((err) => next(err))
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,6 +6,8 @@ const adminController = require('../../controllers/admin-controllers')
 router.get('/restaurant/create', adminController.createRestaurant)
 router.post('/restaurant', adminController.postRestaurant)
 router.get('/restaurant/:id', adminController.getRestaurant)
+router.get('/restaurant/:id/edit', adminController.editRestaurant)
+router.put('/restaurant/:id', adminController.putRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
