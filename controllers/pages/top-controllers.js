f4111c19df37e4ad37cfe7d23451adb8f87d8bfd
jameshoo123 <hoosinyong@hotmail.com>
2022-06-17 01:10:03 +0800

    feat: optimize sequelize of getTopUsers

--- controllers/pages/top-controllers.js
+++ controllers/pages/top-controllers.js
@@ -1,30 +1,49 @@
-const {
-  User,
-  Comment,
-  Restaurant,
-  Favorite,
-  Like,
-  Followship,
-  sequelize,
-} = require('../../models')
+const { User, Restaurant, sequelize } = require('../../models')
 const helpers = require('../../helpers/auth-helpers')
 
 const topController = {
   getTopUsers: async (req, res, next) => {
     try {
+      const userId = req.user.id
+
       let users = await User.findAll({
-        include: [{ model: User, as: 'Followers' }],
+        attributes: [
+          'id',
+          'name',
+          'image',
+          [
+            sequelize.literal(
+              '(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.following_id = User.id)'
+            ),
+            'followerCounts',
+          ],
+          [
+            sequelize.literal(
+              `EXISTS (SELECT follower_id FROM Followships WHERE Followships.following_id = User.id AND Followships.Follower_id  = ${userId})`
+            ),
+            'isFollowed',
+          ],
+        ],
+        order: [[sequelize.col('followerCounts'), 'DESC']],
+        limit: 10,
       })
 
-      users = users
-        .map((user) => ({
-          ...user.toJSON(),
-          followerCount: user.Followers.length,
-          isFollowed: req.user.Followings.some((f) => f.id === user.id),
-        }))
-        .sort((a, b) => b.followerCount - a.followerCount)
+      users = users.map((user) => ({ ...user.toJSON() }))
 
-      res.render('top-users', { users: users })
+      console.log(users)
+      // let users = await User.findAll({
+      //   include: [{ model: User, as: 'Followers', attributes: ['id'] }],
+      // })
+
+      // users = users
+      //   .map((user) => ({
+      //     ...user.toJSON(),
+      //     followerCount: user.Followers.length,
+      //     isFollowed: req.user.Followings.some((f) => f.id === user.id),
+      //   }))
+      //   .sort((a, b) => b.followerCount - a.followerCount)
+
+      res.render('top-users', { users })
     } catch (err) {
       next(err)
     }
