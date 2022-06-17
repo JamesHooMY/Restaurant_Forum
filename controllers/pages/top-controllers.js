b6eb0c7f25abb4d36ebb1636f6b874583b5d540e
jameshoo123 <hoosinyong@hotmail.com>
2022-06-17 10:23:18 +0800

    feat: optimize sequelize of getTopRestaurants

--- controllers/pages/top-controllers.js
+++ controllers/pages/top-controllers.js
@@ -4,7 +4,7 @@ const helpers = require('../../helpers/auth-helpers')
 const topController = {
   getTopUsers: async (req, res, next) => {
     try {
-      const userId = req.user.id
+      const userId = helpers.getUser(req).id
 
       let users = await User.findAll({
         attributes: [
@@ -50,26 +50,62 @@ const topController = {
   },
   getTopRestaurants: async (req, res, next) => {
     try {
-      const restaurants = await Restaurant.findAll({
-        include: [{ model: User, as: 'FavoritedUsers' }],
+      const userId = helpers.getUser(req).id
+
+      let restaurants = await Restaurant.findAll({
+        attributes: [
+          'id',
+          'name',
+          'image',
+          'description',
+          [
+            sequelize.literal(
+              '(SELECT COUNT(DISTINCT id) FROM Favorites WHERE Favorites.restaurant_id = Restaurant.id)'
+            ),
+            'favoritedUserCounts',
+          ],
+          [
+            sequelize.literal(
+              `EXISTS (SELECT user_id FROM Favorites WHERE Favorites.restaurant_id = Restaurant.id AND Favorites.user_id  = ${userId})`
+            ),
+            'isFavorited',
+          ],
+        ],
+        order: [[sequelize.col('favoritedUserCounts'), 'DESC']],
+        limit: 10,
       })
 
-      const userId = helpers.getUser(req).id
-      const limit = 10
+      restaurants = restaurants.map((restaurant) => ({
+        ...restaurant.toJSON(),
+      }))
 
-      const result = restaurants
-        .map((restaurant) => ({
-          ...restaurant.toJSON(),
-          description: restaurant.description.substring(0, 50),
-          favoritedCount: restaurant.FavoritedUsers.length,
-          isFavorited: restaurant.FavoritedUsers.some(
-            (user) => Number(user.id) === Number(userId)
-          ),
-        }))
-        .sort((a, b) => b.favoritedCount - a.favoritedCount)
-        .slice(0, limit)
+      // const restaurants = await Restaurant.findAll({
+      //   include: [
+      //     {
+      //       model: User,
+      //       as: 'FavoritedUsers',
+      //       attributes: [],
+      //       through: { attributes: [] }, // 不加這個，下一層還包含了 Followships 資料
+      //     },
+      //   ],
+      // })
 
-      res.render('top-restaurants', { restaurants: result })
+      // const userId = helpers.getUser(req).id
+      // const limit = 10
+
+      // const result = restaurants
+      //   .map((restaurant) => ({
+      //     ...restaurant.toJSON(),
+      //     description: restaurant.description.substring(0, 50),
+      //     favoritedCount: restaurant.FavoritedUsers.length,
+      //     isFavorited: restaurant.FavoritedUsers.some(
+      //       (user) => Number(user.id) === Number(userId)
+      //     ),
+      //   }))
+      //   .sort((a, b) => b.favoritedCount - a.favoritedCount)
+      //   .slice(0, limit)
+
+      res.render('top-restaurants', { restaurants })
     } catch (err) {
       next(err)
     }
